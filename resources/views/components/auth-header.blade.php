2ffeeade7e294fbc871a9c3d6b76ff6f92cfeff2
ananda wahyan <wahyanabdillah@gmail.com>
2024-06-14 19:23:52 +0700

    inisiasi

---
+++ resources/views/components/auth-header.blade.php
@@ -0,0 +1,6 @@
+ <div class="login-brand">
+     <img src="{{ asset('img/sipkes/logoSipkes.svg') }}"
+         alt="logo"
+         width="100"
+         class="shadow-light rounded-circle">
+ </div>
